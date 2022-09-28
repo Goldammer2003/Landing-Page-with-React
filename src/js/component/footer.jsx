50b54e059c88622a3a451c7020ee788305be7647
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-09-28 16:44:02 +0000

    Added the server.py file

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,7 +2,9 @@ import React from "react";
 const Footer =  () => {
     return (
         <div id= "Footer">
-            <span></span>
+            <span className="position-absolute top-50 start-50 translade-middle">Copyright your website</span>
         </div>
     )
 }
+
+export default Footer
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types":
+import Footer from "";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
+
 const Home = () => {
+	return (
+		<div>
+			<div className ="container-fluid"></div>
+		</div>
+	)
+}
+
+
+
+
+
+
+
+
+
+
+
+{const Home = () => {
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
@@ -21,6 +43,7 @@ const Home = () => {
 			</p>
 		</div>
 	);
-};
+};}
 
 export default Home;
+
