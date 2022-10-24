a3c31f58947fadcbcc2c3373efced1e4ff6daf35
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-10-24 13:00:31 +0000

    Great Progress

---
+++ src/js/pages/home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Navbar from "../component/navbar.jsx";
+import Jumbotron from "../component/jumbotron.jsx";
+import Card from "../component/card.jsx";
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+
+
+const Home = () => {
+	return (
+		<div>
+            <Navbar/>
+			<div className ="container-fluid">
+                <Jumbotron/>
+                <div className ="row">
+                <div className="col-lg-3 col-md-6 col-sm-12 justify-content-center d-flex "><Card/> </div>
+                <div className="col-lg-3 col-md-6 col-sm-12 justify-content-center d-flex"><Card/> </div>
+                <div className="col-lg-3 col-md-6 col-sm-12 justify-content-center d-flex"><Card/> </div>
+                <div className="col-lg-3 col-md-6 col-sm-12 justify-content-center d-flex"><Card/> </div>
+                </div>
+            </div>
+		</div>
+	)
+}
+
+export default Home;
+
