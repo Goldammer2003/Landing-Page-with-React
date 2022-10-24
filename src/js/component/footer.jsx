2315ecb67d80993d95cbf4a0e82b4268927731a9
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-10-24 18:48:23 +0000

    Margin top added so there is more space between the footer and the cards

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,7 +1,7 @@
 import React from "react"; 
 const Footer =  () => {
     return (
-      <div className ="d-flex justify-content-center p-3 bg-dark">
+      <div className ="d-flex justify-content-center p-3 bg-dark mt-5">
         <p className="text-white">Copyright@yourWEbsite2019</p>
       </div>
 
