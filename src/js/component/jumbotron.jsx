ce01a358f0387f0d409c68a1b3de12d5c81abc64
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-10-24 15:47:52 +0000

    Little change in the jumbotron file

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -5,9 +5,9 @@ export default function Jumbotron() {
     <div>
         <div className="p-5 mb-4 bg-light rounded-3">
       <div className="container-fluid py-5">
-        <h1 className="display-5 fw-bold ">Custom jumbotron</h1>
-        <p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-        <button className="btn btn-primary btn-lg" type="button">Example button</button>
+        <h1 className="display-5 fw-bold ">A Warm Welcome!</h1>
+        <p className="col-md-8 fs-4">This text is just an example text, showing that i used an Jumbotron. Since we use React the jumbotron is one component we work with, since we use React </p>
+        <button className="btn btn-primary btn-lg" type="button">Call to action!</button>
       </div>
     </div>
     </div>
